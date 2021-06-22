92b1264411228cb02be161b01c4e5662a6f3cbda
hungcurry <ooopp42@gmail.com>
2021-06-22 08:30:54 +0800

    更新API網址

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 const apiPath = 'ooopp42';
-const url = 'https://hexschoollivejs.herokuapp.com';
+// const url = 'https://hexschoollivejs.herokuapp.com';
+const url = 'https://livejs-api.hexschool.io';
 const token = 'ARKZV2RDkgPePxwyRbN09mcoR1s2';
